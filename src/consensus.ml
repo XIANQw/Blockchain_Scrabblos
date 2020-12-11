ca4a3ae3b6ba8964fde5bd5b216e3a6d7fd0c4e4
XIANQw <qiwei-xian@qq.com>
2020-12-11 23:49:43 +0100

    finish function head

--- src/consensus.ml
+++ src/consensus.ml
@@ -1,33 +1,86 @@
 open Word
 open Constants
+open Letter
 
 (* ignoring unused variables - to be removed *)
 let _ = ignore genesis
 
 (* end ignoring unused variables - to be removed *)
 
-let word_score { word; _ } : int =
-  (* ignoring unused variables - to be removed *)
-  ignore word ;
-  (* end ignoring unused variables - to be removed *)
-  (* TODO *)
-  assert false
 
-let fitness st word =
-  (* ignoring unused variables - to be removed *)
-  ignore st ;
-  ignore word ;
-  ignore word_score ;
-  (* end ignoring unused variables - to be removed *)
-  (* TODO *)
-  assert false
+let letter_score l : int =
+  match l.letter with
+  | 'a' -> 1
+  | 'b' -> 3
+  | 'c' -> 3
+  | 'd' -> 2
+  | 'e' -> 1
+  | 'f' -> 4
+  | 'g' -> 2
+  | 'h' -> 4
+  | 'i' -> 1
+  | 'j' -> 8
+  | 'k' -> 5
+  | 'l' -> 1
+  | 'm' -> 3
+  | 'n' -> 1
+  | 'o' -> 1
+  | 'p' -> 3
+  | 'q' -> 10
+  | 'r' -> 1
+  | 's' -> 1
+  | 't' -> 1
+  | 'u' -> 1
+  | 'v' -> 4
+  | 'w' -> 4
+  | 'x' -> 8
+  | 'y' -> 4
+  | 'z' -> 10
+  | _ -> 0
 
-(* TODO *)
+
+let word_score (word : word) : int =
+  let rec recur (wordContent : letter list) res : int =
+    match wordContent with
+    | [] -> 0
+    | (e::tl) -> recur tl (res + (letter_score e))
+  in recur word.word 0
+
+
+let fitness st word = 
+  let rec fitness hash score =
+    let word  = Store.get_word st hash in
+      if word.level = 0 then 
+        score 
+      else fitness (word.head) (score + (word_score word))
+  in fitness (Word.hash word) 0
+
+let choose word1  word2 st = 
+  let word1,word2 = Option.get word1,Option.get word2 in
+  let fit1,fit2 = fitness st word1,fitness st word2 in
+    if fit1 > fit2 then 
+      Some word1 
+    else if fit1 < fit2 then
+      Some word2 
+    else if word1.signature < word2.signature then 
+      Some word1
+    else if word1.signature > word2.signature then 
+      Some word2
+    else  failwith "fail head"
+  
 
 let head ?level (st : Store.word_store) =
-  (* ignoring unused variables - to be removed *)
-  ignore level ;
-  ignore st;  
-  (* end ignoring unsed variables - to be removed *)
-  (* TODO *)
-  assert false;
+  Hashtbl.fold 
+      (fun _ (word:Word.word) w ->
+        if Option.get level != word.level then
+          w 
+        else if w = None then 
+          Some(word) 
+        else 
+          choose (Some word) w st) 
+      (Store.get_words_table st)
+      None
+
+
+
+
