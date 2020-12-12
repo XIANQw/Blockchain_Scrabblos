04abd0bfb13f8ca9a0bec964e61236cd7043c6d5
XIANQw <qiwei-xian@qq.com>
2020-12-12 15:32:34 +0100

    added function generate_new_word_by_letters and complete run

--- src/politicien.ml
+++ src/politicien.ml
@@ -1,6 +1,9 @@
 open Messages
 open Word
 open Crypto
+open Letter
+open Dictionary
+
 
 type politician = { sk : Crypto.sk; pk: Crypto.pk}  [@@deriving yojson, show]
 
@@ -22,13 +25,45 @@ let make_word_on_blockletters level letters politician head : word =
   let head_hash = Crypto.hash head in
   make_word_on_hash level letters politician head_hash
 
+
+(* interleave 1 [2;3] = [ [1;2;3]; [2;1;3]; [2;3;1] ] *)
+let rec interleave x lst = 
+  match lst with
+  | [] -> [[x]]
+  | hd::tl -> (x::lst) :: (List.map (fun y -> hd::y) (interleave x tl))
+
+(*permutations [1; 2; 3] = [[1; 2; 3]; [2; 1; 3]; [2; 3; 1]; [1; 3; 2]; [3; 1; 2]; [3; 2; 1]] *)
+let rec permutations lst = 
+  match lst with
+  | hd::tl -> List.concat (List.map (interleave hd) (permutations tl))
+  | _ -> [lst]
+;;
+
+let generate_new_word_by_letters (letters:letter list) = 
+  let allCombinations = permutations letters in
+  let rec parcours (combinations :letter list list) =
+    match combinations with
+    | [] -> None
+    | combination :: tl -> 
+      if Dictionary.contains combination then Some combination else parcours tl
+  in parcours allCombinations
+
+
 let send_new_word st level =
-  Option.iter ( fun (head:word) ->
-  let lettersFromStore = Store.get_letters st.letter_store head.head in
-  let word = make_word_on_blockletters level lettersFromStore st.politician (Word.to_bigstring head) in
-  let message = Messages.Inject_word word in Client_utils.send_some message
+  let letters = List.map snd 
+  (List.of_seq (Hashtbl.to_seq (Store.get_letters_table st.letter_store))) in
+  let cur_letters = List.filter (fun (letter:letter) -> letter.level = level) letters in
+  let res = generate_new_word_by_letters cur_letters in
+  if res != None then (
+    Option.iter (fun (head:word) ->
+      let word = make_word_on_blockletters level 
+        (Option.get res) 
+        st.politician 
+        (Word.to_bigstring head) in
+      let message = Messages.Inject_word word in Client_utils.send_some message
+    )
+    (Consensus.head ~level:(level - 1) st.word_store)
   )
-  (Consensus.head ~level:(level - 1) st.word_store)
 
 
 let run ?(max_iter = 0) () =
@@ -76,15 +111,46 @@ let run ?(max_iter = 0) () =
 
   (* start listening to server messages *)
   Log.log_warn "Start listening to server messages";
+  Client_utils.send_some Messages.Listen;
   
   (*  main loop *)
   let level = ref wordpool.current_period in
-  ignore level;
-  failwith ("a programmer" ^ __LOC__)
+  let rec loop max_iter =
+    if (max_iter = 0) then ()
+    else (
+      match Client_utils.receive () with
+      | Messages.Inject_word word -> (** Command server to inject a word *)
+          Store.add_word storeWords word;
+          let st = {politician=state.politician; 
+                    word_store=storeWords; 
+                    letter_store=state.letter_store; 
+                    next_words = state.next_words} in
+          Option.iter (
+            fun (head:word) ->
+              if head = word then send_new_word st !level
+          ) (Consensus.head ~level:(!level - 1) storeWords)
+      | Messages.Inject_letter letter -> (** Command server to inject a letter *)
+        Store.add_letter storeLetters letter;
+        let st = {politician=state.politician; 
+                  word_store=state.word_store; 
+                  letter_store=storeLetters; 
+                  next_words = state.next_words} in
+        Option.iter 
+          (fun (head : word) -> 
+            Log.log_info "head is %a @." Word.pp head; 
+            send_new_word st !level
+          )
+          (Consensus.head ~level:(!level-1) storeWords)
+      | Messages.Next_turn next -> level := next; Log.log_info "Next turn";
+      |_ -> ();
+    loop (max_iter - 1)
+    );
+  in loop max_iter
   
 
 let _ =
   let main =
+    Log.log_info "Start politician";
     Random.self_init () ;
     let () = Client_utils.connect () in
     run ~max_iter:(-1) ()
