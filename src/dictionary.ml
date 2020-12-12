429b1549f84f07c77e147ecf809c65f73de2e455
XIANQw <qiwei-xian@qq.com>
2020-12-12 15:32:09 +0100

    modify function contains

--- src/dictionary.ml
+++ src/dictionary.ml
@@ -1,10 +1,16 @@
+open Letter
+
 let dictionary =
   (** Should concat list 1_10 and 5_15 *) 
   [ Client_utils.list_of_dict "dict/dict_100000_1_10.txt" @ Client_utils.list_of_dict "dict/dict_100000_5_15.txt"]
 @ [Client_utils.list_of_dict "dict/dict_100000_25_75.txt"]
-@ [Client_utils.list_of_dict "dict/dict_100000_50_200"]
+@ [Client_utils.list_of_dict "dict/dict_100000_50_200.txt"]
 
-let contains str =
+let contains (letters: letter list) =
+  let str = List.fold_left (fun str letter -> 
+    let letterStr = String.make 1 letter.letter in
+    letterStr ^ str
+  ) "" letters in
   let length = String.length str in
   if length >= 1 && length <= 15 then
     List.mem str (List.nth dictionary 0)
@@ -13,5 +19,3 @@ let contains str =
   else if length >= 50 && length <= 200 then
     List.mem str (List.nth dictionary 2)
   else false
-
-
