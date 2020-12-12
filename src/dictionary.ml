2efda7251d3981210d86fc96107704cbf8205c33
XIANQw <qiwei-xian@qq.com>
2020-12-12 11:04:11 +0100

    add dictionary

---
+++ src/dictionary.ml
@@ -0,0 +1,17 @@
+let dictionary =
+  (** Should concat list 1_10 and 5_15 *) 
+  [ Client_utils.list_of_dict "dict/dict_100000_1_10.txt" @ Client_utils.list_of_dict "dict/dict_100000_5_15.txt"]
+@ [Client_utils.list_of_dict "dict/dict_100000_25_75.txt"]
+@ [Client_utils.list_of_dict "dict/dict_100000_50_200"]
+
+let contains str =
+  let length = String.length str in
+  if length >= 1 && length <= 15 then
+    List.mem str (List.nth dictionary 0)
+  else if length >= 25 && length <= 75 then
+    List.mem str (List.nth dictionary 1)
+  else if length >= 50 && length <= 200 then
+    List.mem str (List.nth dictionary 2)
+  else false
+
+
