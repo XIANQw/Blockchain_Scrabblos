d6e51595818974e0d474a4ebe6d71ccf30d687df
XIANQw <qiwei-xian@qq.com>
2020-12-12 11:35:10 +0100

    modify random_char to just generate lowercase latin letter

--- src/author.ml
+++ src/author.ml
@@ -9,7 +9,8 @@ let make_letter_on_block sk pk level block letter : letter =
   let head_hash = hash block in
   make_letter_on_hash sk pk level head_hash letter
 
-let random_char () = Random.int (122 - 65) + 65 |> Char.chr
+(** Just generate lowercase latin letter*)
+let random_char () = Random.int (122 - 97) + 97 |> Char.chr
 
 let send_new_letter sk pk level store =
   (* Get blockchain head *)
