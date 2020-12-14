8a24e19d7acb4ecc2dad1ab2345aa98074b14b72
XIANQw <qiwei-xian@qq.com>
2020-12-14 02:58:03 +0100

    fixed send_new_word

--- src/politicien.ml
+++ src/politicien.ml
@@ -1,19 +1,13 @@
-open Messages
+(* open Messages *)
 open Word
 open Crypto
-open Letter
-open Dictionary
 
-
-type politician = { sk : Crypto.sk; pk: Crypto.pk}  [@@deriving yojson, show]
-
-let make_politician (sk:Crypto.sk) (pk:Crypto.pk) =
-  {sk:sk; pk:pk}
+type politician = { sk : Crypto.sk; pk : Crypto.pk } [@@deriving yojson, show]
 
 type state = {
   politician : politician;
   word_store : Store.word_store;
-  letter_store : Store.letter_store;
+  mutable letter_store : Store.letter_store;
   next_words : word list;
 }
 
@@ -22,11 +16,11 @@ let make_word_on_hash level letters politician head_hash : word =
   Word.make ~word:letters ~level ~pk:politician.pk ~sk:politician.sk ~head
 
 let make_word_on_blockletters level letters politician head : word =
-  let head_hash = Crypto.hash head in
+  let head_hash = hash head in
   make_word_on_hash level letters politician head_hash
 
 
-(* interleave 1 [2;3] = [ [1;2;3]; [2;1;3]; [2;3;1] ] *)
+  (* interleave 1 [2;3] = [ [1;2;3]; [2;1;3]; [2;3;1] ] *)
 let rec interleave x lst = 
   match lst with
   | [] -> [[x]]
@@ -39,13 +33,13 @@ let rec permutations lst =
   | _ -> [lst]
 ;;
 
-let generate_new_word_by_letters (letters:letter list) = 
+let generate_new_word_by_letters (letters:Letter.letter list) = 
   let allCombinations = permutations letters in
-  let rec parcours (combinations :letter list list) =
+  let rec parcours (combinations :Letter.letter list list) =
     match combinations with
     | [] -> None
     | combination :: tl -> 
-      if contains combination then Some combination 
+      if Dictionary.contains combination then Some combination 
       else parcours tl
   in parcours allCombinations
 
@@ -65,53 +59,35 @@ let generate_new_word level state =
       (Option.get valid_letters) 
       state.politician
       (Word.to_bigstring word))
+  
+let is_word_optimal word_store word =
+  let optimal = ref word in
+  let words = Store.get_words_table word_store in
+  let word_list = List.of_seq (Hashtbl.to_seq words) in
+  List.iter (fun (_, mot) ->
+    let scoreWord = Consensus.fitness word_store word in
+    let scoreMot = Consensus.fitness word_store mot in
+    if((mot.level = word.level) &&  (scoreMot > scoreWord)) then optimal := mot
+    else () 
+  ) word_list; !optimal = word
+  
+let send_new_word state level =
+  let word_op = generate_new_word level state in
+  match word_op with
+  |None -> Log.log_info "Generate word failed@."
+  |word_op -> let word = Option.get word_op in
+  Store.add_word state.word_store word;
+  (** Inject the word if and only if word has the highest score *)
+  if is_word_optimal state.word_store word then (
+    Log.log_info "Best word is %a@." Word.pp_word word;
+    Client_utils.send_some (Messages.Inject_word word);
+  )
 
-
-(* let send_new_word state level =
-  let letters = List.map snd 
-  (List.of_seq (Hashtbl.to_seq (Store.get_letters_table state.letter_store))) in
-  let cur_letters = List.filter (fun (letter:letter) -> letter.level = level) letters in
-  let res = generate_new_word_by_letters cur_letters in
-  if Option.is_some res then (
-    let letterlist = Option.get res in
-    Option.iter (fun (head:word) ->
-      let word = make_word_on_blockletters level 
-        letterlist 
-        state.politician 
-        (Word.to_bigstring head) in
-      Client_utils.send_some (Messages.Inject_word word)
-    )
-    (Consensus.head ~level:(level - 1) state.word_store)
-  ) *)
-
-  let is_word_optimal word_store word =
-    let optimal = ref word in
-    let words = Store.get_words_table word_store in
-    let word_list = List.of_seq (Hashtbl.to_seq words) in
-    List.iter (fun (_, mot) ->
-      let scoreWord = Consensus.fitness word_store word in
-      let scoreMot = Consensus.fitness word_store mot in
-      if((mot.level = word.level) &&  (scoreMot > scoreWord)) then optimal := mot
-      else () 
-    ) word_list; !optimal = word
-    
-  let send_new_word state level =
-    let word_op = generate_new_word level state in
-    match word_op with
-    |None -> Log.log_info "Generate word failed@."
-    |word_op -> let word = Option.get word_op in
-    (** Inject the word if and only if word has the highest score *)
-    if is_word_optimal state.word_store word then (
-      Log.log_info "Best word is %a@." Word.pp_word word;
-      Store.add_word (state.word_store) word
-    )
-
-let run ?(max_iter = 0) () =
+  let run ?(max_iter = 0) () =
 
   (* Generate public/secret keys *)
   Log.log_warn " Generate public/secret keys " ;
   let (pk, sk) = Crypto.genkeys () in
-  Client_utils.send_some (Messages.Register pk);
   let politician = {sk:sk; pk:pk} in
   
   (* Get initial wordpool *)
@@ -146,7 +122,7 @@ let run ?(max_iter = 0) () =
               word_store = storeWords; 
               letter_store = storeLetters; 
               next_words=[]} in
-  send_new_word state wordpool.current_period;
+  send_new_word state (wordpool.current_period - 1) ;
 
   (* start listening to server messages *)
   Log.log_warn "Start listening to server messages";
@@ -155,10 +131,11 @@ let run ?(max_iter = 0) () =
   (*  main loop *)
   let level = ref wordpool.current_period in
   let finish = ref false in
+  
   let rec loop max_iter =
     if (max_iter = 0) then ()
     else (
-      match Client_utils.receive () with
+      (match Client_utils.receive () with
       | Messages.Inject_word word -> (** Recieve command of inject a word *)
         Log.log_info "******** Inject word %a *******@." Word.pp word ;  
         Store.add_word storeWords word;
@@ -169,20 +146,22 @@ let run ?(max_iter = 0) () =
       | Messages.Next_turn p -> 
         if( p < 0) then finish := true
         else level := p; Log.log_info "Next turn %d" p;
-      |_ -> ();
+      |_ -> ()
+      );
       if (!finish) then () else loop (max_iter - 1)
     )
-  in loop max_iter;
-
+    in
+    loop max_iter;
+    
   (** Stop listening after loop *)
   Client_utils.send_some Messages.Stop_listen;
   (** Find the winner *)
   let winner = Consensus.win storeWords in
   Log.log_info "winner is %a@." Crypto.pp_pk winner
 
+
 let _ =
   let main =
-    Log.log_info "Start politician";
     Random.self_init () ;
     let () = Client_utils.connect () in
     run ~max_iter:(-1) ()
